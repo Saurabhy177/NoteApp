61b58b6351c605144e58735d6b10f722b4a8206b
Saurabh Yadav <saurabh.yadav@skuad.io>
2022-02-28 01:47:23 +0530

    notes screen done

--- app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteOrder.kt
@@ -4,4 +4,13 @@ sealed class NoteOrder(val orderType: OrderType) {
     class Title(orderType: OrderType): NoteOrder(orderType)
     class Date(orderType: OrderType): NoteOrder(orderType)
     class Color(orderType: OrderType): NoteOrder(orderType)
+
+    // Creating our own copy() method to allow change of order type.
+    fun copy(orderType: OrderType): NoteOrder {
+        return when(this) {
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+            is Color -> Color(orderType)
+        }
+    }
 }
